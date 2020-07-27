24716d87e3a10041b20e3c36b08ae27edd960aeb
Kitecraft Unborn <kitecraft@gmail.com>
2020-07-27 12:28:01 -0600

    Set default vref to an even 1100.

--- ESP32AnalogRead_VoltageDividerExample.ino
+++ ESP32AnalogRead_VoltageDividerExample.ino
@@ -1,7 +1,7 @@
 #include <driver/adc.h>
 #include "esp_adc_cal.h"
 
-#define DEFAULT_VREF    1102        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES   50          //Multisampling
 
 // Voltage divider example.
